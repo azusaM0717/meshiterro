3fe0022e6a757cb96668f68be9f5fd857c0cea6f
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-12 12:42:43 +0000

    [start]app2-20

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'homes/about', to: 'homes#about', as: 'about'
   resources :post_images,only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   
