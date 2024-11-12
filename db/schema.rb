4bd52210c1e67776c6115a7d5d0ee260366f02bf
AzusaMorishita <184392623+azusaM0717@users.noreply.github.com>
2024-11-12 05:47:44 +0000

    [start]app2-18

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_12_010257) do
+ActiveRecord::Schema.define(version: 2024_11_12_044203) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2024_11_12_010257) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -48,8 +48,7 @@ ActiveRecord::Schema.define(version: 2024_11_12_010257) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "post_images", id: false, force: :cascade do |t|
-    t.integer "id"
+  create_table "post_images", force: :cascade do |t|
     t.string "shop_name"
     t.text "caption"
     t.integer "user_id"
